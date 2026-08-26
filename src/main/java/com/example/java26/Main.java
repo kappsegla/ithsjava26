6ae9ad628b1382ac473193b94f6505151af286c6
Martin Blomberg <martin.blomberg@outlook.com>
2026-08-26 14:30:45 +0200

    Prints a message to send code to GitHub

--- src/main/java/com/example/java26/Main.java
+++ src/main/java/com/example/java26/Main.java
@@ -3,5 +3,6 @@ package com.example.java26;
 public class Main {
     public static void main(String[] args) {
         IO.println("Hello World!");
+        IO.println("Send new code to github");
     }
 }

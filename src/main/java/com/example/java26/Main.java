44af140781304f04e7d587476c82391f61faf0d0
Martin Blomberg <martin.blomberg@outlook.com>
2026-08-26 14:22:36 +0200

    Revert "Adds a new println to Main"
    This reverts commit 4504725040cab4adfe5ce69d55653f8a9335b0f3.

--- src/main/java/com/example/java26/Main.java
+++ src/main/java/com/example/java26/Main.java
@@ -3,6 +3,5 @@ package com.example.java26;
 public class Main {
     public static void main(String[] args) {
         IO.println("Hello World!");
-        IO.println("Git will save this.");
     }
 }

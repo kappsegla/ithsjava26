e0eae3fbe0910b9107543959c355f20e2e44415a
Martin Blomberg <martin.blomberg@outlook.com>
2026-09-03 15:35:58 +0200

    Adds `makesTen` and `equalSlices` methods to `Exercise9To18` class and improves code formatting

--- src/main/java/com/example/java26/exercises/Exercise9To18.java
+++ src/main/java/com/example/java26/exercises/Exercise9To18.java
@@ -7,12 +7,11 @@ public class Exercise9To18 {
         int sum = sum(10, 10);
         double celcius = 20.0;
         double fahrenheit = 0.0;
-        IO.println("Temperature is " + celcius + " ℃ and " + String.format("%.1f",toFahrenheit(celcius)) + " ℉");
-        IO.println("Temperature is " + fahrenheit + " ℉ and " + String.format("%.1f",toCelcius(fahrenheit)) +" ℃");
+        IO.println("Temperature is " + celcius + " ℃ and " + String.format("%.1f", toFahrenheit(celcius)) + " ℉");
+        IO.println("Temperature is " + fahrenheit + " ℉ and " + String.format("%.1f", toCelcius(fahrenheit)) + " ℃");
 
         fizzBuzz(5);
         IO.println(fizzBuzz2(3));
-
     }
 
     private static double toCelcius(double fahrenheit) {
@@ -26,38 +25,49 @@ public class Exercise9To18 {
     public static int sum(int a, int b) {
         return a + b;
     }
+
     //Comparing primitive types can be done using ==
     public static boolean compare(int a, int b) {
         return a == b;
     }
+
     //Comparing object types must use equals
     public static boolean compare(String a, String b) {
         return a.equals(b);
     }
 
-    public static boolean divisibleByFive(int number){
+    public static boolean divisibleByFive(int number) {
         return number % 5 == 0;
     }
 
     public static void fizzBuzz(int number) {
-        if(number % 3 == 0 && number % 5 == 0)
+        if (number % 3 == 0 && number % 5 == 0)
             IO.println("FizzBuzz");
-        else if( number % 3 == 0)
+        else if (number % 3 == 0)
             IO.println("Fizz");
-        else if( number % 5 == 0)
+        else if (number % 5 == 0)
             IO.println("Buzz");
         else
             IO.println(number);
     }
 
     public static String fizzBuzz2(int number) {
-        if(number % 3 == 0 && number % 5 == 0)
+        if (number % 3 == 0 && number % 5 == 0)
             return "FizzBuzz";
-        else if( number % 3 == 0)
+        else if (number % 3 == 0)
             return "Fizz";
-        else if( number % 5 == 0)
+        else if (number % 5 == 0)
             return "Buzz";
         else
             return "" + number;
     }
+
+    public static boolean makesTen(int num1, int num2) {
+        return num1 == 10 || num2 == 10 || num1 + num2 == 10;
+    }
+
+    public static int equalSlices(int totalSlices, int recipients) {
+        return totalSlices / recipients;
+    }
+
 }

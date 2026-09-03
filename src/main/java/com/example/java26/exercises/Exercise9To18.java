7400e18d72a01f5848498f32e629117b605f5df6
Martin Blomberg <martin.blomberg@outlook.com>
2026-09-03 14:54:23 +0200

    Adds comparison, divisibility, and FizzBuzz methods to `Exercise9To18` class

--- src/main/java/com/example/java26/exercises/Exercise9To18.java
+++ src/main/java/com/example/java26/exercises/Exercise9To18.java
@@ -6,9 +6,13 @@ public class Exercise9To18 {
         IO.println(sum(10, 10));
         int sum = sum(10, 10);
         double celcius = 20.0;
-        double fahrenheit = 20.0;
-        IO.println("Temperature is " + celcius + " ℃ and " + toFahrenheit(celcius) + " ℉");
-        IO.println("Temperature is " + fahrenheit + " ℉ and " + toCelcius(fahrenheit) +" ℃");
+        double fahrenheit = 0.0;
+        IO.println("Temperature is " + celcius + " ℃ and " + String.format("%.1f",toFahrenheit(celcius)) + " ℉");
+        IO.println("Temperature is " + fahrenheit + " ℉ and " + String.format("%.1f",toCelcius(fahrenheit)) +" ℃");
+
+        fizzBuzz(5);
+        IO.println(fizzBuzz2(3));
+
     }
 
     private static double toCelcius(double fahrenheit) {
@@ -22,6 +26,38 @@ public class Exercise9To18 {
     public static int sum(int a, int b) {
         return a + b;
     }
+    //Comparing primitive types can be done using ==
+    public static boolean compare(int a, int b) {
+        return a == b;
+    }
+    //Comparing object types must use equals
+    public static boolean compare(String a, String b) {
+        return a.equals(b);
+    }
 
+    public static boolean divisibleByFive(int number){
+        return number % 5 == 0;
+    }
 
+    public static void fizzBuzz(int number) {
+        if(number % 3 == 0 && number % 5 == 0)
+            IO.println("FizzBuzz");
+        else if( number % 3 == 0)
+            IO.println("Fizz");
+        else if( number % 5 == 0)
+            IO.println("Buzz");
+        else
+            IO.println(number);
+    }
+
+    public static String fizzBuzz2(int number) {
+        if(number % 3 == 0 && number % 5 == 0)
+            return "FizzBuzz";
+        else if( number % 3 == 0)
+            return "Fizz";
+        else if( number % 5 == 0)
+            return "Buzz";
+        else
+            return "" + number;
+    }
 }

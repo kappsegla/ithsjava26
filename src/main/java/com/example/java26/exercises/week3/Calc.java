e9f4add151963a4dce45ea155264d1ce89015b1d
Martin Blomberg <martin.blomberg@outlook.com>
2026-09-10 14:50:53 +0200

    Extends `Calc` class with method overloading for various sum operations

--- src/main/java/com/example/java26/exercises/week3/Calc.java
+++ src/main/java/com/example/java26/exercises/week3/Calc.java
@@ -2,10 +2,43 @@ package com.example.java26.exercises.week3;
 
 public class Calc {
     static void main() {
-
+        IO.println(sum(2, 2));
+        IO.println(sum(2, 2, 2));
+        IO.println(sum(new int[]{2, 2, 2, 2}));
+        IO.println(sum("22"));
     }
 
-    static int sum(){
-        return 0;
+    static int sum(int a, int b) {
+        return a + b;
+    }
+
+    static int sum(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    static int sum(int[] nums) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        return sum;
+        //Alt
+        // return Arrays.stream(nums).sum();
+    }
+
+    static int sum(String numbers) {
+        int sum = 0;
+        for (int i = 0; i < numbers.length(); i++) {
+            if (Character.isDigit(numbers.charAt(i)))
+                //Alt    sum += Integer.parseInt(numbers.substring(i, i + 1));
+                //Alt sum += numbers.charAt(i) - '0';
+                sum += Character.getNumericValue(numbers.charAt(i));
+        }
+        //Alt without loop
+//        sum = numbers.chars()
+//                .filter(Character::isDigit)
+//                .map(Character::getNumericValue)
+//                .sum();
+        return sum;
     }
 }

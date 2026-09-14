ba64d86b9c667a02e5bffe13771c6c4fd0d629ce
Martin Blomberg <martin.blomberg@outlook.com>
2026-09-14 14:42:24 +0200

    Adds passenger validation in `Vehicle` class and updates `VehicleRegister` with new passenger manipulation example

--- src/main/java/com/example/java26/oop/Vehicle.java
+++ src/main/java/com/example/java26/oop/Vehicle.java
@@ -7,13 +7,15 @@ public class Vehicle {
     private double mpg;
 
     public Vehicle() {
-        model = "Unknown";
+        model = "Unknown";  //Alt: Use an empty String with ""
         passengers = 1;
         fuelCap = 1;
         mpg = 1;
     }
 
     public Vehicle(String model, int passengers, double fuelCap, double mpg) {
+        if (passengers < 0)
+            throw new IllegalArgumentException("Passengers cannot be negative");
         this.model = model;
         this.passengers = passengers;
         this.fuelCap = fuelCap;
@@ -25,6 +27,8 @@ public class Vehicle {
     }
 
     public void setPassengers(int passengers) {
+        if (passengers < 0)
+            throw new IllegalArgumentException("Passengers cannot be negative");
         this.passengers = passengers;
     }
 
@@ -43,11 +47,12 @@ public class Vehicle {
     public void setMpg(double mpg) {
         this.mpg = mpg;
     }
+
     public double getMpg() {
         return mpg;
     }
 
-    public double computeRange(){
-        return mpg*fuelCap;
+    public double computeRange() {
+        return mpg * fuelCap;
     }
 }
--- src/main/java/com/example/java26/oop/VehicleRegister.java
+++ src/main/java/com/example/java26/oop/VehicleRegister.java
@@ -1,12 +1,29 @@
 package com.example.java26.oop;
 
+import java.util.Vector;
+
 public class VehicleRegister {
     static void main() {
         Vehicle vehicle = new Vehicle();
         Vehicle vehicle2 = new Vehicle("Volvo V70", 4, 60, 35);
 
-        IO.println(vehicle.getModel());
-        IO.println(vehicle2.getModel());
+//        IO.println(vehicle.getModel());
+//        IO.println(vehicle.getPassengers());
+//
+//        IO.println(vehicle2.getModel());
+//        IO.println(vehicle2.getPassengers());
 
+        Vehicle vehicle3 = vehicle2;
+        vehicle3.setPassengers(10);
+
+        IO.println(vehicle2.getPassengers());
+        IO.println(vehicle3.getPassengers());
+
+        increaseNumberOfPassengers(vehicle3);
+        IO.println(vehicle3.getPassengers());
+    }
+
+    public static void increaseNumberOfPassengers(Vehicle vehicle) {
+        vehicle.setPassengers(vehicle.getPassengers() + 1);
     }
 }
